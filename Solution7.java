cc725d4b13321f1294e59a4159c202e0d4cae217
Balubalaji04 <mallinenibalaji2004@gmail.com>
2024-06-13 07:30:36 -0700

    Add files via upload

---
+++ Solution7.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution7 {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=sc.nextInt();
+        System.out.print(a+b+c);
+        
+    }
+}
